860189ea988714d6d2d71fa68759e82a70926bf7
Andrew <AndrewMelad>
2023-02-20 17:05:25 +0200

    add type written text

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,9 +3,11 @@
     class="header-section text-center space-y-7 p-10 dark:text-white"
     id="main"
   >
-    <p class="text-xl">Hello, I'm</p>
-    <h1 class="text-5xl capitalize text-teal-600 font-black">
-      andrew melad dawwood
+    <h1 class="text-4xl font-bold">
+      <p class="mb-10 text-teal-400">Hi, I'm</p>
+      <span class="typed-text text-teal-400">{{ typeValue }}</span>
+      <span class="blinking-cursor">|</span>
+      <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
     </h1>
     <h3 class="capitalize text-2xl">front end developer</h3>
     <p class="leading-loose capitalize">
@@ -69,10 +71,53 @@
 export default {
   name: "headerSection",
   data() {
-    return {};
+    return {
+      typeValue: "",
+      typeStatus: false,
+      displayTextArray: ["Andrew Melad Dawood", "Andrew Melad Dawood"],
+      typingSpeed: 100,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      displayTextArrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+  created() {
+    setTimeout(this.typeText, this.newTextDelay + 200);
   },
   methods: {
-    type() {},
+    typeText() {
+      if (
+        this.charIndex <
+        this.displayTextArray[this.displayTextArrayIndex].length
+      ) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.displayTextArray[
+          this.displayTextArrayIndex
+        ].charAt(this.charIndex);
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.displayTextArray[
+          this.displayTextArrayIndex
+        ].substring(0, this.charIndex - 1);
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        this.displayTextArrayIndex += 1;
+        if (this.displayTextArrayIndex >= this.displayTextArray.length)
+          this.displayTextArrayIndex = 0;
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    },
   },
 };
 </script>
